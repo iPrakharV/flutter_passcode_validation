386969e0e42bffaa08f61ea2209b23c466a52b5d
Prakhar Verma <connect@iprakharv.com>
2024-04-29 16:14:36 -0400

    Imported dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pw_validator/flutter_pw_validator.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MainApp());
